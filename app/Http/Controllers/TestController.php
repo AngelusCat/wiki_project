87a2b24838bf496af05d499cb582c6cfe98c43d3
Dr.Angelicus <Kittyhellokitty555@gmail.com>
2024-04-22 19:59:27 +0300

    API

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,13 +9,15 @@ class TestController extends Controller
 {
     public function index()
     {
-/*        $response = Http::get("https://ru.wikipedia.org/w/api.php", [
-            "action" => "opensearch",
-            "search" => "Достоевский,_Фёдор_Михайлович",
-            "limit" => "5",
-            "namespace" => "0",
+/*        $response = Http::get("https://en.wikipedia.org/w/api.php", [
+            "action" => "parse",
+            "page" => "100_gecs",
             "format" => "json"
         ]);
-        dump($response->collect());*/
+        dump($response->body());*/
+
+        //https://www.mediawiki.org/wiki/Manual:Installing_MediaWiki/ru
+        //https://www.mediawiki.org/wiki/Extension:TextExtracts#API
+        //https://www.mediawiki.org/wiki/API:Get_the_contents_of_a_page
     }
 }
