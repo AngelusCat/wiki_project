b718b8a8b544e60e46ed39492834aef1b5e7ffba
Dr.Angelicus <Kittyhellokitty555@gmail.com>
2024-04-23 14:26:05 +0300

    Добавить данные в таблицы

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,19 +9,35 @@ class TestController extends Controller
 {
     public function index()
     {
+
+        $titles = "Достоевский,_Фёдор_Михайлович";
+
+        //Пробелы в названии заменять нижнем подчеркиванием
+
         $response = Http::get("https://ru.wikipedia.org/w/api.php", [
             "action" => "query",
             "prop" => "extracts",
-            //"exsentences" => 10,
             "exlimit" => 1,
-            "titles" => "Достоевский,_Фёдор_Михайлович",
+            "titles" => $titles,
             "explaintext" => 1,
             "format" => "json"
             //api.php?action=query&prop=extracts&exsentences=10&exlimit=1&titles=Pet_door&explaintext=1&formatversion=2 [try in ApiSandbox]
         ]);
-        $test = json_decode($response->body(), JSON_OBJECT_AS_ARRAY)["query"]["pages"][1456]["extract"];
 
-        $replaceArray = [
+        // Сделать проверку: найдена статья или нет, если нет, то сообщить об этом пользователю, иначе продолжить
+        // обработку
+
+        /*$test = json_decode($response->body(), JSON_OBJECT_AS_ARRAY);
+        $testOne = $test['query']['pages'];
+        $key = array_key_last($testOne);
+        $test = $test['query']['pages'][$key]['extract'];*/
+
+        $articleContent = json_decode($response->body(), JSON_OBJECT_AS_ARRAY);
+        $pages = $articleContent['query']['pages'];
+        $key = array_key_last($pages);
+        $articleContent = $pages[$key]['extract'];
+
+        $replacementArray = [
             "А́" => "А",
             "а́" => "а",
             "Е́" => "Е",
@@ -41,22 +57,31 @@ class TestController extends Controller
             "Я́" => "Я",
             "я́" => "я"
         ];
-
-        //dump(preg_replace('/(А́|а́|Е́|е́|И́|и́|О́|о́|У́|у́|Ы́|ы́|Э́|э́|Ю́|ю́|Я́|я́)/u', '*$0*', $test));
-
-        //Посчитать количество вхождений слова-атома в тексте статьи
-        $result = mb_substr_count($test, 'от', 'utf8');
-
         //Заменяет буквы с ударением на их аналоги без ударения
-        $test2 = strtr($test, $replaceArray);
+        $articleContent = strtr($articleContent, $replacementArray);
+
+        //Получить ссылку на статью
+        $link = 'https://ru.wikipedia.org/wiki/' . $titles;
 
         //Разбить текст статьи на слова-атомы
-        $test3 = preg_split('/[^а-яёА-ЯЁ0-9]+/u', $test2, -1, PREG_SPLIT_NO_EMPTY);
+        $wordsAtoms = preg_split('/[^а-яёА-ЯЁ0-9]+/u', $articleContent, -1, PREG_SPLIT_NO_EMPTY);
+
+        //Посчитать размер
+        $size = 0;
+
+        foreach ($wordsAtoms as $word) {
+            $size += strlen($word);
+        }
+
+        $kbyte = 1024;
+
+        $size = (int) round($size/$kbyte);
 
         //Посчитать количество вхождений каждого слова-атома
-        $test4 = array_count_values($test3);
+        $numberOfOccurrencesOfWord = array_count_values($wordsAtoms);
 
-        dump($test4);
+        //Посчитать количество слов в статье
+        $numberOfWordsInArticle = array_sum($wordsAtoms);
 
         /**
          * exlimit
@@ -79,6 +104,8 @@ class TestController extends Controller
          */
 
         //http://www.mysql.ru/docs/man/Using_InnoDB_tables.html
+
+        //ENGINE=InnoDB CHARACTER SET utf8;
 
         //https://www.mediawiki.org/wiki/API:Get_the_contents_of_a_page
         //https://www.mediawiki.org/wiki/Extension:TextExtracts#API
