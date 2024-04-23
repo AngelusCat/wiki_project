c15d111ff16d7b59c174710d5a1a5bba365fb617
Dr.Angelicus <Kittyhellokitty555@gmail.com>
2024-04-23 07:18:16 +0300

    API plain text

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,15 +9,19 @@ class TestController extends Controller
 {
     public function index()
     {
-/*        $response = Http::get("https://en.wikipedia.org/w/api.php", [
-            "action" => "parse",
-            "page" => "100_gecs",
+        $response = Http::get("https://en.wikipedia.org/w/api.php", [
+            "action" => "query",
+            "prop" => "extracts",
+            "exsentences" => 10,
+            "exlimit" => 1,
+            "titles" => "Pet_door",
+            "explaintext" => 1,
             "format" => "json"
+            //api.php?action=query&prop=extracts&exsentences=10&exlimit=1&titles=Pet_door&explaintext=1&formatversion=2 [try in ApiSandbox]
         ]);
-        dump($response->body());*/
+        dump($response->body());
 
-        //https://www.mediawiki.org/wiki/Manual:Installing_MediaWiki/ru
-        //https://www.mediawiki.org/wiki/Extension:TextExtracts#API
         //https://www.mediawiki.org/wiki/API:Get_the_contents_of_a_page
+        //https://www.mediawiki.org/wiki/Extension:TextExtracts#API
     }
 }
