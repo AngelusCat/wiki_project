0152f70f24f75242881442a25bff2db525f55b0b
Dr.Angelicus <Kittyhellokitty555@gmail.com>
2024-04-27 10:29:41 +0300

    Добавить результат обработки

--- resources/views/wiki/import2.blade.php
+++ resources/views/wiki/import2.blade.php
@@ -3,6 +3,7 @@
     <input type="text" name="articleName">
     <input type="submit" value="Скопировать">
 </form>
+<br><br>
 <script>
     var form = document.getElementById('form');
     var params = new FormData(form);
@@ -13,11 +14,31 @@
 </script>
 <div>
     @if(isset($articles))
-        @foreach($articles as $article)
+{{--        @foreach($articles as $article)
             <p>Название: {{$article->title}}</p>
             <p>Ссылка: {{$article->link}}</p>
             <p>Размер: {{$article->size}}</p>
             <p>Количество слов: {{$article->word_count}}</p>
-        @endforeach
+        @endforeach--}}
+        <table border="5">
+            <thead>
+                <tr>
+                    <th>Название статьи</th>
+                    <th>Ссылка</th>
+                    <th>Размер статьи</th>
+                    <th>Кол-во статьи</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($articles as $article)
+                    <tr>
+                        <td @class(['test2' => $loop->even])>{{$article->title}}</td>
+                        <td @class(['test2' => $loop->even])>{{$article->link}}</td>
+                        <td @class(['test2' => $loop->even])>{{$article->size}}</td>
+                        <td @class(['test2' => $loop->even])>{{$article->word_count}}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     @endif
 </div>
