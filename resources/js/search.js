1ee1630a618e730afebf2485b1c6c996f85e773b
Dr.Angelicus <160015645+AngelusCat@users.noreply.github.com>
2024-05-01 06:18:21 +0300

    Update search.js

--- resources/js/search.js
+++ resources/js/search.js
@@ -1,12 +1,12 @@
 const elem = document.querySelectorAll('a');
-const preArticleContent = document.getElementById('articleContent');
+const divArticleContent = document.getElementById('articleContent');
 for ( let i = 0; i < elem.length; i++ ) {
     elem[i].addEventListener( 'click', function () {
         let title = elem[i].name;
         let url = '/getArticleContent/' + title;
         fetch(url).then(function (response) {
             response.text().then(function (text) {
-                preArticleContent.innerHTML = text;
+                divArticleContent.innerHTML = text;
             });
         });
     });
