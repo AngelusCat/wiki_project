0926768175286dc1115a2dfc564d5d8308e03c5c
Dr.Angelicus <Kittyhellokitty555@gmail.com>
2024-05-08 10:26:47 +0300

    l

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::match(['get', 'post'], '/', [\App\Http\Controllers\WikiParserController::class, 'import']);
+/*Route::match(['get', 'post'], '/', [\App\Http\Controllers\WikiParserController::class, 'import']);
 Route::match(['get', 'post'], '/search', [\App\Http\Controllers\WikiParserController::class, 'search']);
 Route::get('/importHTMLCode', [\App\Http\Controllers\WikiParserController::class, 'getImportHTMLCode']);
 Route::get('/searchHTMLCode', [\App\Http\Controllers\WikiParserController::class, 'getSearchHTMLCode']);
-Route::get('/getArticleContent/{title}', [\App\Http\Controllers\WikiParserController::class, 'getArticleContent'])->where(['title' => '.*']);
+Route::get('/getArticleContent/{title}', [\App\Http\Controllers\WikiParserController::class, 'getArticleContent'])->where(['title' => '.*']);*/
+
+Route::get('/', [\App\Http\Controllers\TestController::class, 'test']);
