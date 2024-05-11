3825dcf26eb695eea609f26b83600814b07eb12a
Dr.Angelicus <Kittyhellokitty555@gmail.com>
2024-05-11 10:15:58 +0300

    Изменить условие вызова getTable

--- resources/js/test/index.js
+++ resources/js/test/index.js
@@ -2,6 +2,19 @@ let importTab = document.getElementById('importTab');
 let searchTab = document.getElementById('searchTab');
 let content = document.getElementById('content');
 
+let children = content.children;
+let childrenArray = Array.prototype.slice.call(children);
+let flag = false;
+
+for (let elem of childrenArray) {
+    let id = elem.id;
+    if (id === 'table') {
+        flag = true;
+    }
+}
+
+console.log(flag);
+
 importTab.addEventListener('click', function() {
     let htmlCode = fetch('/importHTML').then(function(htmlCode) {
         htmlCode.text().then(function(text) {
